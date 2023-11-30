8f3fa5f771f5c7fc67f09ab0bd70521d604521a0
Aleeykhan <92122352+Aleeykhan@users.noreply.github.com>
2023-11-30 09:41:34 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+class MSGStart {
+    constructor() {
+        this._Start_ = this._Start_.bind(this);
+    }
+    _Start_() {
+        console.log("Messanger App is started...");
+        return;
+    }
+}
+class Messanger extends MSGStart {
+    constructor(name, version) {
+        super();
+        this.name = name;
+        this.version = version;
+        this.Send = this.Send.bind(this);
+        this.Details = this.Details.bind(this);
+        this._Start_();
+        this.Details();
+    }
+    Send(message) {
+        console.log("[*] FROM: " + this.name + ", MESSAGE: " + message);
+    }
+    Details() {
+        console.log("[*] NAME: " + this.name + "\n[*] VERSION: " + this.version);
+    }
+}
